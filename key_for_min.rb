1297a81e414a71a7cd8170db0c1d92596ef69094
Jesse Summer-Lavigne <jessesl22@gmail.com>
2020-05-13 20:52:48 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,5 +6,6 @@ def key_for_min_value(name_hash)
   lowest_value = nil 
   
   name_hash.each do |key, value|
-    if lowest_value = nil || lowest_key < lowest_value
+    if lowest_value = nil || value < lowest_value
+      
 end
